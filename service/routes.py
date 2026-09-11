b729789633e6cddca9cb0af3b73ac5eeb424c015
Dineshr46 <dineshrdinesh11@gmail.com>
2026-09-10 21:10:24 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -56,6 +56,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -87,7 +88,7 @@ def get_accounts(account_id):
 
     account = Account.find(account_id)
     if not account:
-       abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
 
@@ -111,6 +112,7 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # DELETE AN ACCOUNT
